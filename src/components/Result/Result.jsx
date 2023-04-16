8f28da6b22e8d24d8ca8316bd6f98b961c9a63ee
Joekin Roche <joekinroches@gmail.com>
2023-04-16 20:22:18 +0530

    Result page API fetch

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,103 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Result.css";
-import IMG from "../../assets/sample.jpg"
-
-const Results = [
-  {
-    video_id: 1,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-  {
-    video_id: 2,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-  {
-    video_id: 3,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-  {
-    video_id: 4,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-  {
-    video_id: 5,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-  {
-    video_id: 6,
-    thumbnail_url: "url",
-    ball_type: "Full Length",
-    ball_speed: "6 mps",
-    shot_played: "On Drive",
-    shot_selection_accuracy: "100%",
-    other_recommended_shots: ["Cover Drive"],
-  },
-];
 
 export default function Result() {
-//   <section>
-//     <div className="container">
-//       {Results.map(
-//         ({
-//           video_id,
-//           thumbnail_url,
-//           ball_type,
-//           ball_speed,
-//           shot_played,
-//           shot_selection_accuracy,
-//           other_recommended_shots,
-//         }) => {
-          return (
-            <div className="m-[5rem]">
-            <div>
-                <h1 className="project_title font-bold flex justify-center">Analyse Results</h1>
-            </div>
-            <div className="project_container ">    
-            <article key="1" className="project_item">
-              <img className="project_item-image ml-[3.5rem]" src={IMG} />
-              <p className="project_title flex justify-center">Length: Good Length</p>
-              <p className="project_title flex justify-center">Speed: 106</p>
-              <p className="project_title flex justify-center">Shot Played: Cover Drive</p>
-              <p className="project_title flex justify-center">Accuracy: 80%</p>
-              <p className="project_title flex justify-center">Ideal Shot: Straight Drive</p>
-              <iframe width="100" height="100" src="" allowfullscreen></iframe>
+  const [data, setData] = useState(null);
 
-            </article>
-            </div>
-            </div>
-          )
-        //   ;
-//         }
-//       )}
-//     </div>
-    
-//   </section>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://3f0e-2409-4072-d-c198-f5c5-676b-c55e-5054.ngrok-free.app/api/analyze-video"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch video");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("error fetching video");
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="project_Title font-bold flex justify-center">
+          Analyse Results
+        </h1>
+      </div>
+      return (
+      <div className="m-[5rem] project_container">
+        <div className="project_container ">
+          <article key="1" className="project_item">
+            <img
+              className="project_item-image ml-[3.5rem]"
+              src={data.thumbnail_url}
+            />
+            <p className="project_title flex justify-center">
+              Length: {data.ball_type}
+            </p>
+            <p className="project_title flex justify-center">
+              Speed: {data.ball_speed}
+            </p>
+            <p className="project_title flex justify-center">
+              Shot Played: {data.shot_played}
+            </p>
+            <p className="project_title flex justify-center">
+              Accuracy: {data.shot_selection_accuracy}
+            </p>
+            <p className="project_title flex justify-center">
+              Ideal Shot: {data.other_recommended_shots}
+            </p>
+            <iframe
+              width="100"
+              height="100"
+              src={data.url}
+              allowfullscreen
+            ></iframe>
+          </article>
+        </div>
+      </div>
+      ) ;
+    </div>
+  );
 }
 
-<iframe width="480" height="480" src="https://www.youtube.com/embed/WSnQUKBD8g4" allowfullscreen></iframe>
-
+// video_id: 1,
+//     thumbnail_url: "url",
+//     ball_type: "Full Length",
+//     ball_speed: "6 mps",
+//     shot_played: "On Drive",
+//     shot_selection_accuracy: "100%",
+//     other_recommended_shots: ["Cover Drive"],
